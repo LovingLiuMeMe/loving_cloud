ffd0860adf88ea6df26a0692620502d86f568733
lb <cmds12345678@163.com>
2019-12-12 14:05:58 +0800

    后台启动注册中心

--- loving-eureka/start.sh
+++ loving-eureka/start.sh
@@ -3,4 +3,5 @@ mvn clean package -Dmaven.test.skip=true
 echo "============进入target目录==================="
 cd target
 echo "============运行jar包==================="
-java -jar loving-0.0.1-SNAPSHOT.jar
+nohup java -jar loving-0.0.1-SNAPSHOT.jar > log.file 2>&1 &
+
