1bba8a464ff3b4834fcf045213772fdcd123a94d
lb <cmds12345678@163.com>
2019-12-12 15:14:32 +0800

    更改启动脚本

--- loving-product/start.sh
+++ loving-product/start.sh
@@ -1,7 +1,7 @@
 echo "===========编译并跳过单元测试===================="
 mvn clean install -Dmaven.test.skip=true
 echo "============进入target目录==================="
-cd target
+cd product-server/target
 echo "============运行jar包==================="
 nohup java -jar loving-product-server-0.0.1-SNAPSHOT.jar > log.file 2>&1 &
 
